2eaf44d79dd19fc7e482950567150e8f1c65dd0c
Hasyari <hasyari00@gmail.com>
2024-06-20 17:09:22 +0700

    changes

--- latihan3.py
+++ latihan3.py
@@ -1,53 +1,28 @@
-def value(r):
-    if (r == 'I'):
-        return 1
-    if (r == 'V'):
-        return 5
-    if (r == 'X'):
-        return 10
-    if (r == 'L'):
-        return 50
-    if (r == 'C'):
-        return 100
-    if (r == 'D'):
-        return 500
-    if (r == 'M'):
-        return 1000
-    return -1
- 
-def romanToDecimal(str):
-    res = 0
-    i = 0
- 
-    while (i < len(str)):
- 
-        # Getting value of symbol s[i]
-        s1 = value(str[i])
- 
-        if (i + 1 < len(str)):
- 
-            # Getting value of symbol s[i + 1]
-            s2 = value(str[i + 1])
- 
-            # Comparing both values
-            if (s1 >= s2):
- 
-                # Value of current symbol is greater
-                # or equal to the next symbol
-                res = res + s1
-                i = i + 1
+class Solution(object):
+    roman_map = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    }
+    
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        result = self.roman_map[s[-1]]
+        for i in range(len(s) - 2, -1, -1):
+            if self.roman_map[s[i]] >= self.roman_map[s[i + 1]]:
+                result += self.roman_map[s[i]]
             else:
- 
-                # Value of current symbol is greater
-                # or equal to the next symbol
-                res = res + s2 - s1
-                i = i + 2
-        else:
-            res = res + s1
-            i = i + 1
- 
-    return res
- 
-# Driver code
-print("Integer form of Roman Numeral is"),
-print(romanToDecimal("MCMXCIVx"))
+                result -= self.roman_map[s[i]]
+        return result
+
+
+solution = Solution()
+
+print(solution.romanToInt("MCMXCIV"))
