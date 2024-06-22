4f3e3dbbdaf7576f832f53be381768c927279368
Hasyari <hasyari00@gmail.com>
2024-06-23 02:38:12 +0700

    changes

--- longestCommonPrefix.py
+++ longestCommonPrefix.py
@@ -1,7 +1,17 @@
-class Solution(object):
-    def longestCommonPrefix(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: str
-        """
-        
+data_long = ["flower","flow","flight"]
+
+
+# for datas in data_long:
+#     for data in range(len(datas)):
+#         print(datas[data] )
+#     print(datas)
+
+
+
+for datas in data_long:
+    for data in [x for x in data_long if x != datas]:
+        pass
+    print()
+
+
+
